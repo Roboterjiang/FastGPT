7fa7e3e4495a4cf0fea8feeada4150783fbcd7dd
adzqq <zqq@ituji.cn>
2024-08-26 10:16:06 +0800

    引用文件设置无法下载

--- projects/app/src/web/core/dataset/hooks/readCollectionSource.ts
+++ projects/app/src/web/core/dataset/hooks/readCollectionSource.ts
@@ -15,7 +15,9 @@ export function getCollectionSourceAndOpen(collectionId: string) {
       const { value: url } = await getCollectionSource(collectionId);
 
       if (!url) {
-        throw new Error('No file found');
+        setLoading(false);
+        return;
+        // throw new Error('No file found');
       }
 
       if (url.startsWith('/')) {
@@ -42,8 +44,12 @@ export function getAdCollectionSourceAndOpen(fileUrl: string) {
     try {
       setLoading(true);
 
+      console.log('getAdCollectionSourceAndOpen', fileUrl);
+
       if (!fileUrl) {
-        throw new Error('No file found');
+        setLoading(false);
+        return;
+        // throw new Error('No file found');
       }
       if (fileUrl.startsWith('/')) {
         window.open(`${location.origin}${fileUrl}`, '_blank');
